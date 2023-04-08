1b2566cba7c315a580f3aa69636bba9e7e970f31
Botond Antalóczy <zane9907@gmail.com>
2023-04-08 18:12:47 +0200

    hazi06 bugos

---
+++ HAZI/HAZI06/NJCleaner.py
@@ -0,0 +1,46 @@
+import pandas as pd
+
+class NJCleaner:
+    
+    def __init__(self, csv_path):
+        self.data = pd.read_csv(csv_path)
+    
+    def order_by_scheduled_time(self):
+        return self.data.sort_values('scheduled_time')
+    
+    def drop_columns_and_nan(self):
+        self.data = self.data.dropna()
+        self.data = self.data.drop(['from', 'to'], axis=1)
+        return self.data
+    
+    def convert_date_to_day(self):
+        self.data['day'] = pd.to_datetime(self.data['date']).dt.day_name()
+        self.data = self.data.drop(['date'], axis=1)
+        return self.data
+    
+    def convert_scheduled_time_to_part_of_the_day(self):
+        bins = pd.IntervalIndex.from_tuples([(0, 3.99), (4, 7.99), (8, 11.99), (12, 15.99), (16, 19.99), (20, 23.99)])
+        labels = ['late_night', 'early_morning', 'morning', 'afternoon', 'evening', 'night']
+        self.data['part_of_the_day'] = pd.cut(pd.to_datetime(self.data['scheduled_time']).dt.hour, bins=bins, labels=labels, include_lowest=True)
+        self.data = self.data.drop(['scheduled_time'], axis=1)
+        return self.data
+    
+    def convert_delay(self):
+        self.data['delay'] = self.data['delay_minutes'].apply(lambda x: 1 if x >= 5 else 0)
+        return self.data
+    
+    def drop_unnecessary_columns(self):
+        self.data = self.data.drop(['train_id', 'scheduled_time', 'actual_time', 'delay_minutes'], axis=1)
+        return self.data
+    
+    def save_first_60k(self, save_path):
+        self.data.iloc[:60000].to_csv(save_path, index=False)
+    
+    def prep_df(self, save_path='data/NJ.csv'):
+        self.order_by_scheduled_time()
+        self.drop_columns_and_nan()
+        self.convert_date_to_day()
+        self.convert_scheduled_time_to_part_of_the_day()
+        self.convert_delay()
+        self.drop_unnecessary_columns()
+        self.save_first_60k(save_path)
