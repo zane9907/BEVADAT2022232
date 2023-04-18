4bace7d4074a5ddffcd1ad1a15635891edc70592
Botond Antaloczy <Botond.Antaloczy@dmgmori-heitec.hu>
2023-04-18 11:17:43 +0200

    gyak08

--- GYAK/GYAK08/LinearRegressionSkeleton.py
+++ GYAK/GYAK08/LinearRegressionSkeleton.py
@@ -7,32 +7,25 @@ class LinearRegression:
         self.lr = lr
 
     def fit(self, X: np.array, y: np.array):
-    # inicializáljuk a súlyokat, bias-t és a költségfüggvényt
-        self.weights = np.zeros(X.shape[1])
-        self.bias = 0
-        self.cost = []
+        n_samples = X.shape[0]
+        n_features = X.shape[1]
 
-        # gradient descent algoritmus futtatása az epochs számának megfelelően
-        for epoch in range(self.epochs):
-            # kiszámítjuk a predikciókat a jelenlegi súlyok és bias alapján
-            y_pred = np.dot(X, self.weights) + self.bias
+        self.weights = np.zeros(n_features + 1)
 
-            # számítjuk a költségfüggvény értékét és eltároljuk
-            cost = np.mean((y_pred - y) ** 2)
-            self.cost.append(cost)
+        X = np.insert(X, 0, 1, axis=1)
 
-            # számítjuk a gradienteket a súlyok és bias szerint
-            dw = np.dot(X.T, (y_pred - y)) / len(y)
-            db = np.sum(y_pred - y) / len(y)
-
-            # frissítjük a súlyokat és bias-t a gradient descent algoritmus alapján
-            self.weights -= self.lr * dw
-            self.bias -= self.lr * db
+        for _ in range(self.epochs):
+            y_pred = np.dot(X, self.weights)
+            error = y - y_pred
+            gradient = -2 * np.dot(X.T, error) / n_samples
+            self.weights -= self.lr * gradient
+        
+   
 
         
 
     def predict(self, X):
-        # kiszámítjuk a predikciókat a tanult súlyok és bias alapján
-        y_pred = np.dot(X, self.weights) + self.bias
+        X = np.insert(X, 0, 1, axis=1)
+        y_pred = np.dot(X, self.weights)
         return y_pred
-
+        
